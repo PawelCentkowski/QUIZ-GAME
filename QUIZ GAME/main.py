031b04818ed4aee0ac644a567fa492de8f77df2d
Danziger <63114561+PawelCentkowski@users.noreply.github.com>
2023-05-24 22:38:12 +0200

    Update main.py

--- QUIZ GAME/main.py
+++ QUIZ GAME/main.py
@@ -1,4 +1,4 @@
-from settings import*
+from kosz.settings import*
 import tkinter as tk
 from tkinter import messagebox
 import random
@@ -10,6 +10,7 @@ class QuizGame(tk.Tk):
         self.title("Quiz Game")
         self.geometry("1600x800")
         self.p1 =  tk.PhotoImage(file = 'civ_logo.png')
+        self.config(background="#555")
         # Setting icon of master window
         self.iconphoto(False, self.p1)
         self.questions = self.load_questions(questions_file)
@@ -17,7 +18,7 @@ class QuizGame(tk.Tk):
         self.score = 0
         self.create_widgets()
         self.next_question()
-
+    #dowload screen res and justifies the application window
     def screen_initiator( self):
         self.window_width = 1600
         self.window_height = 800
@@ -26,6 +27,7 @@ class QuizGame(tk.Tk):
         self.center_x = int(self.screen_width/2-self.window_width/2)
         self.center_y = int(self.screen_height/2-self.window_height/2)
         self.geometry(f"{self.window_width}x{self.window_height}+{self.center_x}+{self.center_y}")
+
     def load_questions(self, file):
         with open(file, "r") as f:
             lines = f.readlines()
@@ -34,34 +36,49 @@ class QuizGame(tk.Tk):
         return questions
 
     def create_widgets(self):
-        self.label_question = tk.Label(self, text="", wraplength=380)
-        self.label_question.pack(pady=20)
+        self.label_question = tk.Label(self, text="", wraplength=800, font=("Arial", 25))
+        self.label_question.config(background = "#555",padx=20,pady=20, height=5)
+        self.label_question.pack()
 
-        self.entry_answer = tk.Entry(self)
-        self.entry_answer.pack(pady=10)
+        #self.entry_answer = tk.Entry(self)
+        #self.entry_answer.pack(pady=10)
+        
+        #self.question_label = tk.Label(text="", font=("Arial", 20))
+        #self.question_label.pack(pady=20)
+
+        self.option_buttons = []
+        self.create_option_buttons()
 
         self.button_check = tk.Button(self, text="Sprawdź", command=self.check_answer)
         self.button_check.pack(pady=5)
 
-        self.label_score = tk.Label(self, text="Wynik: 0")
-        self.label_score.pack()
-
         self.button_next = tk.Button(self, text="Następne pytanie", command=self.next_question, state=tk.DISABLED)
         self.button_next.pack(pady=10)
+
+        self.label_score = tk.Label(self, text="Wynik: 0")
+        self.label_score.pack()
 
     def next_question(self):
         if self.questions:
             self.current_question = self.questions.pop()
             self.label_question.config(text=self.current_question)
-            self.entry_answer.delete(0, tk.END)
+           #self.entry_answer.delete(0, tk.END)
             self.button_check.config(state=tk.NORMAL)
             self.button_next.config(state=tk.DISABLED)
         else:
             messagebox.showinfo("Koniec gry", f"Twój wynik: {self.score}/{len(self.questions)+self.score}")
             self.destroy()
 
+    def create_option_buttons(self):
+        for _ in range(4):
+            button = tk.Button(text=self.questions[_+1], font=("Arial", 16), width=40)
+            button.config(background="#fff")
+            button.pack(pady=5)
+            self.option_buttons.append(button)
+
     def check_answer(self):
-        answer = self.entry_answer.get()
+        #answer = self.entry_answer.get()
+        answer = "a"
         if answer.lower() == "tak":
             self.score += 1
         self.label_score.config(text=f"Wynik: {self.score}")
